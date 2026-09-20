a07d2279b11ca75d238c628fa2a0432f3573a9dc
lau-ka-ho <asdf913@gmail.com>
2026-09-20 12:49:29 +0800

    Apply rule "Unnecessary imports should be removed"

--- src/test/java/org/apache/sshd/client/KeyStoreSslSshReportTest.java
+++ src/test/java/org/apache/sshd/client/KeyStoreSslSshReportTest.java
@@ -14,7 +14,6 @@ import java.lang.reflect.Member;
 import java.lang.reflect.Method;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
